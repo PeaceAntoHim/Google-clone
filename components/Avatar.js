200207f8464c2ff237caae5648128807110fb42a
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-13 23:00:28 +0700

    building the avatar components

---
+++ components/Avatar.js
@@ -0,0 +1,12 @@
+function Avatar({ url }) {
+    return(
+        <img 
+            className="h-10 cursor-pointer duration-150 transform hover:scale-115"
+            loading="lazy"
+            src={url}
+            alt="Profile picture"
+        />
+    )
+}
+
+export default Avatar;
