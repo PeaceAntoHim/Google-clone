a7a2a50637086727e5653137aed230450915001d
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-13 23:27:17 +0700

    made body section in google clone

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
 import Avatar from '../components/avatar';
+import Image from 'next/image';
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/outline';
 
 export default function Home() {
   return (
@@ -24,6 +27,7 @@ export default function Home() {
          
 
           {/* Icon */}
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
           {/* Avatar */}
             <Avatar 
@@ -32,6 +36,22 @@ export default function Home() {
           </div>
         </header>
       {/* Body */}
+      <form>
+          <Image
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            height={100}
+            width={300}
+          />
+
+          <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md">
+            <SearchIcon className="h-5 mr-3 text-gray-500" />
+            <input 
+              type="text"
+              className="flex-grow focus:outline-none"
+            />
+            <MicrophoneIcon className="h-5" />
+          </div>
+      </form>
 
       {/* Footer */}
 
