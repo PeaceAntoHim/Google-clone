46f72d82063e1c63d8872f3f9d3878d1ff2b0672
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-02-15 10:50:30 +0700

    Building Search page

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,7 +1,7 @@
 function Avatar({ url }) {
     return(
         <img 
-            className="h-10 cursor-pointer duration-150 transform hover:scale-120"
+            className="h-10 cursor-pointer duration-150 transform hover:scale-110"
             loading="lazy"
             src={url}
             alt="Profile picture"
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
 import Head from 'next/head';
-import Avatar from '../components/avatar';
+import { useRef } from 'react'
 import Image from 'next/image';
-import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
-import { SearchIcon } from '@heroicons/react/outline';
+import { useRouter } from 'next/router';
 import Footer from '../components/Footer';
+import Avatar from '../components/avatar';
+import { SearchIcon } from '@heroicons/react/outline';
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
 
 export default function Home() {
+  /* Thsi logic to search */
+  const router = useRouter();
+  const searchInputRef = useRef(null)
+  const search = e => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;  
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -46,16 +60,17 @@ export default function Home() {
 
           <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
             <SearchIcon className="h-5 mr-3 text-gray-500" />
-            <input 
+            <input
               type="text"
+              ref={searchInputRef} 
               className="flex-grow focus:outline-none"
             />
             <MicrophoneIcon className="h-5" />
           </div>
 
           <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
-            <button className="btn">Google Search</button>
-            <button className="btn">I'm Feeling Lucky</button>
+            <button onClick={search} className="btn">Google Search</button>
+            <button onClick={search} className="btn">I'm Feeling Lucky</button>
           </div>
       </form>
 
